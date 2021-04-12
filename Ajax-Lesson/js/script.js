c6b4bbd81ce278f120b32f8ef382568b668520f2
tmy628 <tmy6h1r@gmail.com>
2021-04-12 10:34:59 +0000

    [Add] 通信に成功した場合の処理を追記する

--- Ajax-Lesson/js/script.js
+++ Ajax-Lesson/js/script.js
@@ -9,13 +9,31 @@ $(function(){
       dataType : 'jsonp',
     }).done(function (data){
       //通信成功
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+
     }).fail(function (data) {
       //通信失敗
     });
-
   });
 });
 
-// .done()は通信に成功した場合、.fail()は失敗した場合に記述するメソッド
-// 「通信に成功する」とは、今回はWeb APIが正常に呼び出せたことを意味している
-// どちらのメソッドでも(data)を指定しているが、その引数内に取得した結果が入る（dataは任意の変数名）
+// $('#id名').text(JSONから欲しい値)の形で指定すると、指定したidのテキストを、JSONから受け取った値に変換される
+// 最高気温（temp_max）を取得したい場合は、"data.JSONのオブジェクト名.プロパティ名"で取得している
+
+// $(要素名).attr(属性名,値);と指定すると、img要素にsrc属性とalt属性が追加される
+// これにより、天気のアイコンを表示することができる
+// アイコンを取得するURLは、http://openweathermap.org/img/w/"アイコン名".png
